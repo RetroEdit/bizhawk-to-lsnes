a32cd04b6ad4cf84490a9a299638015393a0576e
RetroEdit <30182911+RetroEdit@users.noreply.github.com>
2020-08-19 08:26:26 +0000

    Add author notice to code

--- convert.py
+++ convert.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+SNES BK2 to LSMV converter
+Originally by TheMas3212
+Ported to Python 3 and partially rewritten by RetroEdit
+"""
 import sys
 import os
 import zipfile
